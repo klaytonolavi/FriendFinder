4bd6c71057b4d7a24211ca4392e66a046ce5608a
Klayton <kliekkio@gmail.com>
2017-09-25 20:59:01 -0700

    last commit before readme

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,32 +2,37 @@
 var friendsData = require("../data/friends.js");
 var _ = require("lodash");
 
+// function within module.exports 
 module.exports = function (app) {
   
+    // getting the friends data with the GET method 
   app.get("/api/friends", function (req, res) {
       res.json(friendsData);
   });
 
-  
+    // post method to check scores between users input on the form and friends in the friends array
   app.post("/api/friends", function (req, res) {
       var scores = req.body.scores;
       var compareScores = [];
 
+        // each friend starts with 0 score
       friendsData.forEach(function (friend) {
           var friendScore = 0;
 
+          // function that calculates friendscore
           friend.scores.forEach(function (score, num) {
               friendScore = friendScore + Math.abs(score - parseInt(scores[num]));
           });
 
+          // gives each friend their own score 
           compareScores.push({
               "friend": friend,
               "score": friendScore
           });
       });
 
-      // Using lodash, sort through all the scores and look for the lowest value to the highest
-      // "Shift()" method removes the first element from an array and returns that element
+      // sorts through all the scores with lodash and looks for the lowest value to the highest
+      // shift removes the first element from the array and returns it
       res.json(_.sortBy(compareScores, "score").shift());
   });
 
